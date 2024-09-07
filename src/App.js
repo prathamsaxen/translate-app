f0bd769ddd5cc0fb11e65863a45cc8de0d0acb4a
prathamsaxen <er.prathamsaxena@gmail.com>
2024-09-07 17:46:00 +0530

    Translate APP

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [inputText, setInputText] = useState("");
+  const [outputText, setOutputText] = useState("");
+
+  const TextConvert = async (text) => {
+    const options = {
+      method: "GET",
+      url: process.env.REACT_APP_API,
+      params: {
+        input_text: text,
+        to_language: "en",
+      },
+      headers: {
+        "x-rapidapi-key": process.env.REACT_APP_API_x_rapidapi_key,
+        "x-rapidapi-host": process.env.REACT_APP_API_x_rapidapi_host,
+      },
+    };
+
+    try {
+      const response = await axios.request(options);
+      setOutputText(response.data.translated_text);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="wrapper">
+        <div className="input">
+          <textarea
+            placeholder="Enter Your Text Here..."
+            value={inputText}
+            onChange={(e) => setInputText(e.target.value)}
+          ></textarea>
+        </div>
+        <div className="output">
+          <textarea placeholder="See Your Output Here..." value={outputText}></textarea>
+        </div>
+      </div>
+        <button onClick={() => TextConvert(inputText)}>Convert</button>
     </div>
   );
 }
